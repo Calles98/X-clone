2f6e7543ccc205f68e923666d76db012584608b6
Rodrigo Calles <joserodrigoc98@gmail.com>
2024-05-15 11:14:12 -0700

    Create pages, routes and components

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-red-400">Hello, world!</h1>
+      <h1 className="text-red-400">Home</h1>
     </div>
   );
 }
