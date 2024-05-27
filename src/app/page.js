07bb619c5dd71118b231e263bcf806347909f964
Rodrigo Calles <joserodrigoc98@gmail.com>
2024-05-27 16:14:43 -0700

    Add react-modal package and update commentModal component

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="max-w-xl mx-auto border-r border-l min-h-screen">
-      <div className="py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
+      <div className="py-2 px-3 sticky top-0  bg-white border-b border-gray-200">
         <h2 className="text-lg sm:text-xl font-bold">Home</h2>
         <Input />
         <Feed />
--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,5 +1,13 @@
-import { modalState } from "@/atom/modalAtom";
-import { app } from "@/firebase";
+"use client";
+
+import {
+  HiOutlineChat,
+  HiOutlineHeart,
+  HiOutlineTrash,
+  HiHeart,
+} from "react-icons/hi";
+
+import { signIn, useSession } from "next-auth/react";
 import {
   collection,
   deleteDoc,
@@ -8,25 +16,20 @@ import {
   onSnapshot,
   serverTimestamp,
   setDoc,
-  Timestamp,
 } from "firebase/firestore";
-import { signIn, useSession } from "next-auth/react";
-import React, { useEffect, useState } from "react";
-import {
-  HiHeart,
-  HiOutlineChat,
-  HiOutlineHeart,
-  HiOutlineTrash,
-} from "react-icons/hi";
+import { app } from "../firebase";
+import { useEffect, useState } from "react";
+import { modalState, postIdState } from "../atom/modalAtom";
 import { useRecoilState } from "recoil";
 
-function Icons({ id, uid }) {
+export default function Icons({ id, uid }) {
   const { data: session } = useSession();
-  const db = getFirestore(app);
   const [isLiked, setIsLiked] = useState(false);
-  const [likes, setLikes] = useState([]);
+  const [likes, setLikes] = useState([]); // [1
   const [open, setOpen] = useRecoilState(modalState);
-
+  const [postId, setPostId] = useRecoilState(postIdState); // [2
+  const [comments, setComments] = useState([]); // [3
+  const db = getFirestore(app);
   const likePost = async () => {
     if (session) {
       if (isLiked) {
@@ -54,6 +57,14 @@ function Icons({ id, uid }) {
     );
   }, [likes]);
 
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      collection(db, "posts", id, "comments"),
+      (snapshot) => setComments(snapshot.docs)
+    );
+    return () => unsubscribe();
+  }, [db, id]);
+
   const deletePost = async () => {
     if (window.confirm("Are you sure you want to delete this post?")) {
       if (session?.user?.uid === uid) {
@@ -63,34 +74,46 @@ function Icons({ id, uid }) {
             window.location.reload();
           })
           .catch((error) => {
-            console.error("Error removing the document: ", error);
+            console.error("Error removing document: ", error);
           });
       } else {
-        alert("You're not authorized to delete this post");
+        alert("You are not authorized to delete this post");
       }
     }
   };
 
   return (
     <div className="flex justify-start gap-5 p-2 text-gray-500">
-      <HiOutlineChat
-        className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:bg-sky-500"
-        onClick={() => setOpen(!open)}
-      />
+      <div className="flex items-center">
+        <HiOutlineChat
+          className="h-8 w-8 cursor-pointer rounded-full  transition duration-500 ease-in-out p-2 hover:text-sky-500 hover:bg-sky-100"
+          onClick={() => {
+            if (!session) {
+              signIn();
+            } else {
+              setOpen(!open);
+              setPostId(id);
+            }
+          }}
+        />
+        {comments.length > 0 && (
+          <span className="text-xs">{comments.length}</span>
+        )}
+      </div>
       <div className="flex items-center">
         {isLiked ? (
           <HiHeart
             onClick={likePost}
-            className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:bg-red-500"
+            className="h-8 w-8 cursor-pointer rounded-full  transition text-red-600 duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100"
           />
         ) : (
           <HiOutlineHeart
             onClick={likePost}
-            className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:bg-red-500"
+            className="h-8 w-8 cursor-pointer rounded-full  transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100"
           />
         )}
         {likes.length > 0 && (
-          <span className={`text-sm ${isLiked && "text-red-600"}`}>
+          <span className={`text-xs ${isLiked && "text-red-600"}`}>
             {likes.length}
           </span>
         )}
@@ -98,12 +121,10 @@ function Icons({ id, uid }) {
 
       {session?.user?.uid === uid && (
         <HiOutlineTrash
+          className="h-8 w-8 cursor-pointer rounded-full  transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100"
           onClick={deletePost}
-          className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:bg-red-500"
         />
       )}
     </div>
   );
 }
-
-export default Icons;
