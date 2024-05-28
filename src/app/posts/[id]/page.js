0d79753fa217c771e61edfd7e9166b50d55a6bef
Rodrigo Calles <joserodrigoc98@gmail.com>
2024-05-28 12:59:35 -0700

    Update post page to display selected posts

--- src/app/posts/[id]/page.js
+++ src/app/posts/[id]/page.js
@@ -1,7 +1,26 @@
+import { app } from "@/firebase";
+import { doc, getDoc, getFirestore } from "firebase/firestore";
+import Link from "next/link";
 import React from "react";
+import { HiArrowLeft } from "react-icons/hi";
+import Post from "@/components/Post";
 
-function PostPage() {
-  return <div>PostPage</div>;
+async function PostPage({ params }) {
+  let data = {};
+  const db = getFirestore(app);
+  const querysnapshot = await getDoc(doc(db, "posts", params.id));
+  data = { ...querysnapshot.data(), id: querysnapshot.id };
+  return (
+    <div className="max-w-xl mx-auto border-r border-l min-h-screen">
+      <div className="flex items-center space-x-2 py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
+        <Link href={"/"} className="hover:bg-gray-100 rounded-full p-2">
+          <HiArrowLeft className="h-5 w-5" />
+          <h2 className="sm:text-lg">Back</h2>
+        </Link>
+      </div>
+      <Post post={data} id={data.id} />
+    </div>
+  );
 }
 
 export default PostPage;
